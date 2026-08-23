62e3308679c6f309fdc679d9166f3c367f5d5e40
Issi <issi.dev.iobroker@gmail.com>
2026-08-23 23:13:14 +0200

    chore: modernize adapter toolchain
    Adopt the workspace build, ESLint 9, current TypeScript configuration, and Node.js 22/24 CI coverage. Update the official io-package schema reference and keep private API samples out of version control.\n\nRefs #45, #116\nSupersedes #216

--- test/mocha.setup.js
+++ test/mocha.setup.js
@@ -14,9 +14,12 @@ process.on('unhandledRejection', (e) => {
 
 // enable the should interface with sinon
 // and load chai-as-promised and sinon-chai by default
-const sinonChai = require('sinon-chai');
-const chaiAsPromised = require('chai-as-promised');
+const sinonChaiModule = require('sinon-chai');
+const chaiAsPromisedModule = require('chai-as-promised');
 const { should, use } = require('chai');
+
+const sinonChai = sinonChaiModule.default ?? sinonChaiModule;
+const chaiAsPromised = chaiAsPromisedModule.default ?? chaiAsPromisedModule;
 
 should();
 use(sinonChai);
